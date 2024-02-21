3e46d208d015d4d7fee306d7fca55b28306b6ba5
Alexey <Alexey@DESKTOP-H2N279O>
2024-02-21 13:14:26 +0300

    Добавьте файлы проекта.

---
+++ AppKiller/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AppKiller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var processesNames = new string[]
+            {
+                "Steam",
+                "steam"
+            };
+            while (true)
+            {                
+                foreach (var process in Process.GetProcesses())
+                {
+                    if (processesNames.Contains(process.ProcessName))
+                    {
+                        process.Kill();
+
+                    }
+                }
+                Thread.Sleep(new TimeSpan(0, 1, 0));
+            }
+        }
+    }
+}
